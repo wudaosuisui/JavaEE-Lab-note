f167a1d4aac186ce102f95b1d74d7da0be6eb086
陈锐 <13081011450@163.com>
2017-10-11 18:00:15 +0800

    Update 1011复习整理1.java

--- 1011复习整理1.java
+++ 1011复习整理1.java
@@ -10,13 +10,6 @@ HttpServletRequest request
       request.getParameter("表单name属性的值")
       //示例
       String userName = request.getParameter("name");
-//----------------cookie
-//建立 cookie数组
-Cookie[] cookies = request.getCookies();
-//获取cookie value
-cookie.getValue()
-//获取cookie name
-cookie.getName()
 
 //--------------------------------------------------------------------响应
 //响应的内容等信息 皆包含在此对象内
@@ -41,6 +34,31 @@ A.equals(b);
 //示例
 userName.equals(cookie.getValue());
       
+//---------------------------------------------------------------------cookie
+            //建立 cookie数组
+            Cookie[] cookies = request.getCookies();
+            //获取cookie value
+            cookie.getValue()
+            //获取cookie name
+            cookie.getName()
+            //获取上一界面传递过来的参数
+		String vname = request.getParameter("name");
+		//创建一个cookie对象 name = vName  ,value = vname
+		Cookie cookie = new Cookie("vName", vname);
+		//设置在客户端保存时间  单位为秒 不设置 则会在用户端关闭时  被删除
+		cookie.setMaxAge(60*60*24*7); 
+		//添加进cookie数组
+		response.addCookie(cookie); 
       
-      
-      
+//---------------------------------------------------------------------session
+
+
+
+
+
+
+
+
+
+
+
